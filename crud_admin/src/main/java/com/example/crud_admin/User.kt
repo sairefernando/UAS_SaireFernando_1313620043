d56ead736bff50474c00e2979210617bc791a203
“sairefernando” <saire.fernando@yahoo.com>
2023-12-26 22:37:49 +0700

    initial commit

---
+++ crud_admin/src/main/java/com/example/crud_admin/User.kt
@@ -0,0 +1,4 @@
+package com.example.crud_admin
+
+data class User(val name : String? = null, val operator : String? = null, val location : String? = null, val phone : String? = null){
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UASSAIRE"
+include(":app")
+include(":crud_admin")
